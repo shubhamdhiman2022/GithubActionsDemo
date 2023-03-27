6415ef9c24eb744492996e29dbe1e3ae03587e16
shubham.dhiman <shubham.dhiman@grazitti.local>
2023-03-27 19:31:44 +0530

    Config path changes

--- src/test/java/automationFramework/Config.java
+++ src/test/java/automationFramework/Config.java
@@ -4,7 +4,7 @@ public class Config {
 	
 	public static String fol_failedScreenshotPath =System.getProperty("user.dir")+"/Failed_Screenshots";
 	public static String fol_passScreenshotPath =System.getProperty("user.dir")+"/Pass_Screenshots";
-	public static String fol_jasonFilePath=System.getProperty("user.dir")+"./DataFiles/TestData.json";
+	public static String fol_jasonFilePath=System.getProperty("user.dir")+"/DataFiles/TestData.json";
 	public static String fol_downloadFilePath=System.getProperty("user.dir")+"\\Downloads";
 	//if getting error then change to System.getProperty("user.dir")+"/Dummy_Test_File"+"/dummy.doc";
 	public static String fol_dummyTestDocFilePath=System.getProperty("user.dir")+"\\Dummy_Test_File"+"\\dummy.doc";
